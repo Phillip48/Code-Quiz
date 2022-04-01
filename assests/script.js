daecf3a25b44e4ec5c0f1a46cd896219ca00f477
Phillip48 <phillippereira02@gmail.com>
2022-04-01 17:10:25 -0400

    Added question array for quiz

--- assests/script.js
+++ assests/script.js
@@ -1,5 +1,6 @@
 // Var for button id
 var startGame = document.getElementById("start-game");
+var timer = document.getElementById("time");
 //
 
 //
@@ -8,4 +9,32 @@ var startGame = document.getElementById("start-game");
 
 //
 
-//
+//questions
+var questions = [
+    {
+        title: "When was rock climbing added to the Olympics?",
+        choices: ["2019", "2020", "2021", "Never was added"],
+        answers: "2020"
+    },
+    {
+        title: "How did Spider-man get his powers?",
+        choices: ["He bought them", "He was born with them", "Tony stank gave it to him", "He was bitten by a spider"],
+        answers: "Bitten by a spider"
+    },
+    {
+        title: "In what year did the Beeteles Band begin",
+        choices: ["1950", "1953", "1956", "1960"],
+        answers: "1956"
+    },
+    {
+        title: "Bootcamp class start at: ",
+        choices: ["9:15am", "10:00am", "9:30am", "I'm not in the military"],
+        answers: "10:00am"
+    },
+    {
+        title: "Who free solo climbed El Cap",
+        choices: ["Alex Honnold", "Adam Ondra", "Tommy Caldwell", "Alexander Megos"],
+        answers: "Alex Honnold"
+
+    }
+];
