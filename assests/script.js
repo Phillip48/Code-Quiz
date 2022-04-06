bcc6ca2900d19c79ccb7381e737bfc66931546bf
Phillip48 <phillippereira02@gmail.com>
2022-04-06 10:17:53 -0400

    Working questions

--- assests/script.js
+++ assests/script.js
@@ -12,32 +12,34 @@ var choicesElement = document.getElementById("choices");
 var initialsElement = document.getElementById("initials");
 var feedBackElement = document.getElementById("feedback");
 
+var highScore = [];
+
 //Questions for Quiz
 var questions = [
     {
         title: "When was rock climbing added to the Olympics?",
         choices: ["2019", "2020", "2021", "Never was added"],
-        answers: "2020"
+        answer: "2020"
     },
     {
         title: "How did Spider-man get his powers?",
         choices: ["He bought them", "He was born with them", "Tony stank gave it to him", "He was bitten by a spider"],
-        answers: "Bitten by a spider"
+        answer: "Bitten by a spider"
     },
     {
         title: "In what year did the Beeteles Band begin",
         choices: ["1950", "1953", "1956", "1960"],
-        answers: "1956"
+        answer: "1956"
     },
     {
         title: "Bootcamp class start at: ",
         choices: ["9:15am", "10:00am", "9:30am", "I'm not in the military"],
-        answers: "10:00am"
+        answer: "10:00am"
     },
     {
         title: "Who free solo climbed El Cap",
         choices: ["Alex Honnold", "Adam Ondra", "Tommy Caldwell", "Alexander Megos"],
-        answers: "Alex Honnold"
+        answer: "Alex Honnold"
 
     }
 ];
@@ -66,61 +68,69 @@ function startQuiz() {
 
     // start time
     clockCountDown();
-    
+
     //show starting time on page
 
     getQuestions();
 }
 
 function getQuestions() {
+    choicesElement.textContent = "";
     // create a variable to get the questions array 
     var titleElement = document.getElementById('question-title');
-    var currentQuestions = questions[questionsIndex];
+    var currentQuestion = questions[questionsIndex];
+    console.log(currentQuestion);
+    titleElement.textContent = currentQuestion.title
 
-    titleElement.textContent = currentQuestions.title
-
-    currentQuestions.choices.forEach(function(choices,i) {
+    currentQuestion.choices.forEach(function (choices, i) {
         var choicesBtn = document.createElement('button');
         choicesBtn.setAttribute("class", "choices");
         choicesBtn.setAttribute("value", choices);
-        choicesBtn.textContent = i+1 + ". " + choices;
+        choicesBtn.textContent = choices;
         choicesElement.appendChild(choicesBtn);
 
-        choicesBtn.onclick = clickQuestions();
     });
     //attach click event to listner
 
     //display the choice on the page
-    
+
 }
 
 // question button clicks // Error code from this function
-function clickQuestions() {
-   // var questionAnswers = questions.answers
-    //var questionChoices = questions.choices
-    var questionsLength = questions.length
-    
+function clickQuestions(event) {
+    var response = document.createElement('h3');
+    if (event.target.matches(".choices")) {
+        // var questionAnswers = questions.answers
+        //var questionChoices = questions.choices
+        var questionsLength = questions.length-1
 
-    // error handling for incorrect answer to question
-    if (this.value !== questions[questionsIndex].answers) {
-        console.log("wrong");
+console.log(event.target.textContent);
+
+        // error handling for incorrect answer to question
+        if (event.target.textContent !== questions[questionsIndex].answer) {
+            // time deduction
+            time -= 15;
+            //Wrong!
+           // response.textContent('Wrong!');
+        }
+       
+        // display new time once deduction is done
+        if (event.target.textContent == questions[questionsIndex].answer) {
+            // time deduction
+            //Correct!
+           // response.textContent('Correct!');
+        } 
+
+        // Move to the next question
+        questionsIndex++;
+
+        // check if all questions are done being asked
+        if (questionsIndex === questionsLength) {
+            endQuiz();
+        } else {
+            getQuestions();
+        }
     }
-    // time deduction
-
-    // display new time once deduction is done
-
-    // flash the right or wrong feedback under the page question 
-
-    // Move to the next question
-    questionsIndex++;
-
-    // check if all questions are done being asked
-    if (questions === questionsLength){
-        endQuiz();
-    } else {
-        getQuestions();
-    }
-    
 }
 
 // end the quiz
@@ -139,7 +149,7 @@ function endQuiz() {
 
     // show final screen
     finalScreen.removeAttribute("class", "hide");
-    
+
     // show final score
     finalScore.removeAttribute("class", "hide");
 
@@ -151,33 +161,35 @@ function clockCountDown() {
     // update the time
     timer = setInterval(function () {
         time--;
-    timerElement.textContent = time;
-    if (timer === 0) {
-        endQuiz();
-    }
-    },1000);
+        timerElement.textContent = time;
+        if (timer === 0) {
+            endQuiz();
+        }
+    }, 1000);
 };
 
 //fix some code in function // delete note when done
 function saveHighScores() {
     // get value from your input box
-    var value = initialsElement.value 
+    var value = initialsElement.value
     // make sure user wrote intials
-    if (value === ""){
+    if (value === "") {
         return
     }
     // local storage.... !Not sure what variable to use!
-    localStorage.setItem("Scores", )
+    localStorage.setItem("Scores", highScore)
     // redirect to highscore page// might have to change link
-    location.href="file:///C:/Users/phill/OneDrive/Documents/UCF/homework/Code-Quiz/highscore.html"
+    location.href = "file:///C:/Users/phill/OneDrive/Documents/UCF/homework/Code-Quiz/highscore.html"
 }
 // key event function
 function enterBtnEvent() {
     // Give enter key ability to save your highscore when key down ???? check code
-   initialsElement.addEventListener("keyup", "enter") 
+    initialsElement.addEventListener("keyup", "enter")
 }
 
 // user clicks button to save their initials
 submitBtn.onclick = saveHighScores;
 // user clicks to begin quiz
 startGame.onclick = startQuiz;
+
+document.querySelector('.wrapper').addEventListener("click", clickQuestions)
